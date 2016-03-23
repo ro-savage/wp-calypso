a774078dcd3a34254b048f961f869b5ca9c2fcbf
George Hotelling <george@hotelling.net>
2016-03-22 21:29:54 -0400

    Add Cancel button to Menu editor
    The only way to revert changes to the menu editor was to navigate away from the
    page. This adds a button which will fetch the saved menu from the server. I
    used menu-save-button.jsx as a template for the new button.
    
    This uses the Button component instead of DOM <button>.  This way we get any
    improvements to that button, plus we get to remove an external dependency
    (classNames) at the expense of adding an internal one.
    
    The edit button was wrapping at mobile widths and at 660px to 706px. Mobile is
    easy: add a breakpoint. The other one was tougher. The solution was to change
    the width of the element to a calc() value that provided space for the button.
    
    This was originally avoided in the pre-oss repo commit 0ef153c because
    `text-overflow: ellipsis` did not support `width: calc()`. Browsers appear to
    have improved since then.

---
+++ client/my-sites/menus/menus-revert-button.jsx
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import debugFactory from 'debug';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+
+const debug = debugFactory( 'calypso:menus:revert-button' ); // eslint-disable-line no-unused-vars
+
+const MenuRevertButton = React.createClass( {
+	componentWillMount() {
+		this.props.menuData.on( 'saved', this.setReverted );
+		this.props.menuData.on( 'error', this.setReverted );
+	},
+
+	componentWillUnmount() {
+		this.props.menuData.off( 'saved', this.setReverted );
+		this.props.menuData.off( 'error', this.setReverted );
+	},
+
+	getInitialState() {
+		return { reverting: false };
+	},
+
+	setReverting() {
+		this.setState( { reverting: true } );
+	},
+
+	setReverted() {
+		this.setState( { reverting: false } );
+	},
+
+	revert() {
+		this.setReverting();
+		return this.props.menuData.discard();
+	},
+
+	render() {
+		const { hasChanged } = this.props.menuData.get();
+
+		return (
+			<Button disabled={ ! hasChanged }
+					onClick={ this.revert }>
+				{ this.state.reverting ? this.translate( 'Canceling…' ) : this.translate( 'Cancel' ) }
+			</Button>
+		);
+	}
+} );
+
+export default MenuRevertButton;
